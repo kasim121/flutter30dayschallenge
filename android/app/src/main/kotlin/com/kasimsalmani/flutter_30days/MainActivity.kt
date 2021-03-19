7e890911e806fa6bd73f0cee155ba90e74272250
kasim121 <kasimplecoder@gmail.com>
2021-03-19 13:59:02 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/kasimsalmani/flutter_30days/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kasimsalmani.flutter_30days
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: Home(),
+    );
+  }
+}
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter 30 Days Challenge!'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child : Text("Hello World!"),
+      )
+    );
+  }
+}
+
